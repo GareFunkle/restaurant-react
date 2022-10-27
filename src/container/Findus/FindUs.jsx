ed5cff74a74490b1c1e593d52919d8cc02197835
GarFunkle <vincent.durret@gmail.com>
2022-10-27 12:09:25 +0200

    Finish this project

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React from "react";
+
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className="app__bg app__wrapper section__padding" id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className="headtext__cormorant" style={{ marginBottom: "3rem" }}>
+        Nous Retrouvez
+      </h1>
+      <div className="app__wrapper-content">
+        <p className="p__opensans">
+          Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+        </p>
+        <p
+          className="p__cormorant"
+          style={{ color: "#dcca87", margin: "2rem 0" }}
+        >
+          Horaire d'Ouverture
+        </p>
+        <p className="p__opensans">Lun - Ven: 10:00 H - 14:00 H</p>
+        <p className="p__opensans">Sam - Dim: 10:00 H - 15:00 H</p>
+      </div>
+      <button type="button" className="custom__button" style={{ marginTop: '2rem' }} > En Savoir Plus</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="findus" />
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Nous Contactez</h1>
+        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
+        <p className="p__opensans">+1 212-344-1230</p>
+        <p className="p__opensans">+1 212-555-1230</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">&quot;La meilleure façon de se retrouver est de se perdre au service des autres.&quot;</p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Heure de Travail</h1>
+        <p className="p__opensans">Lundi-Vendredi:</p>
+        <p className="p__opensans">08:00 H - 12:00 H</p>
+        <p className="p__opensans">Samedi-Dimanche:</p>
+        <p className="p__opensans">07:00 H - 11:00 H</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Gericht. All Rights reserved.</p>
+    </div>
+
   </div>
 );
 
